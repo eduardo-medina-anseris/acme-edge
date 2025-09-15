7fd3a367bf5ebe9d3215f0e06b81798e62b01f89
eduardo.medina <eduardo.medina@net-effect.com>
2025-09-15 12:25:25 -0600

    Render banner

--- blocks/banner/banner.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  block.textContent = '';
-  const elem = document.createElement('div');
-  elem.innerHTML = 'Hola soy el banner';
-  block.append(elem);
-}
