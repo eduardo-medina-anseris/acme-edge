39b24c0abc27a6056846ceebcdfa1f58c5c10cb2
eduardo.medina <eduardo.medina@net-effect.com>
2025-09-26 17:16:11 -0600

    Custom button decorator

---
+++ blocks/acc-button/acc-button.js
@@ -0,0 +1,31 @@
+export default function function decorate(a) {
+  a.title = a.title || a.textContent;
+  if (a.href !== a.textContent) {
+    const up = a.parentElement;
+    const twoup = a.parentElement.parentElement;
+    if (!a.querySelector('img')) {
+      if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+        a.className = 'acc-button'; // default
+        up.classList.add('button-container');
+      }
+      if (
+        up.childNodes.length === 1
+        && up.tagName === 'STRONG'
+        && twoup.childNodes.length === 1
+        && twoup.tagName === 'P'
+      ) {
+        a.className = 'button primary';
+        twoup.classList.add('button-container');
+      }
+      if (
+        up.childNodes.length === 1
+        && up.tagName === 'EM'
+        && twoup.childNodes.length === 1
+        && twoup.tagName === 'P'
+      ) {
+        a.className = 'button secondary';
+        twoup.classList.add('button-container');
+      }
+    }
+  }
+}
