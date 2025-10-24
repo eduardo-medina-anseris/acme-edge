76da71b018ab969bcce848fcb15a452cd42d3a9c
eduardo.medina <eduardo.medina@net-effect.com>
2025-10-24 14:14:59 -0600

    Update theme styles - 2025-10-24 14:14:58

--- blocks/acc-button/variants.js
+++ blocks/acc-button/variants.js
@@ -1,10 +1,47 @@
 export default function getVariants() {
-  return {
-    orange: {
-      typography: '',
+  return 
+  {
+    "orange": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/orange",
+      "isDefault": "false",
+      "name": "orange",
+      "title": "Orange",
+      "resourceType": "themes/components/elements/acc/button"
     },
-    black: {
-      typography: '',
+    "purple": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/purple",
+      "isDefault": "false",
+      "name": "purple",
+      "title": "Purple",
+      "resourceType": "themes/components/elements/acc/button"
     },
-  };
+    "black": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/black",
+      "isDefault": "false",
+      "name": "black",
+      "title": "Black",
+      "resourceType": "themes/components/elements/acc/button"
+    },
+    "white-outline": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/white-outline",
+      "isDefault": "false",
+      "name": "white-outline",
+      "title": "White Outline",
+      "resourceType": "themes/components/elements/acc/button"
+    },
+    "white": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/white",
+      "isDefault": "false",
+      "name": "white",
+      "title": "White",
+      "resourceType": "themes/components/elements/acc/button"
+    },
+    "outline": {
+      "path": "/etc/customthemes/clientlibs/acme/acme/jcr:content/components/themes-components-elements-acc-button/outline",
+      "isDefault": "false",
+      "name": "outline",
+      "title": "Outline",
+      "resourceType": "themes/components/elements/acc/button"
+    }
+  }
 }
