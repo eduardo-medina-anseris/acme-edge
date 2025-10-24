8c4fa85f92ed62b4f0eb362c2c37a0a62d7482a6
eduardo.medina <eduardo.medina@net-effect.com>
2025-10-23 20:35:24 -0600

    Test mount point

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'no-param-reassign': [2, { props: false }], // allow modifying properties of param,
+    'xwalk/no-orphan-collapsible-fields': 'off',
   },
 };
