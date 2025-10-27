d392593d2ab80a5590341e04913c6bef4f5bdcdf
eduardo.medina <eduardo.medina@net-effect.com>
2025-10-27 09:45:13 -0600

    Update theme styles - 2025-10-27 09:45:13

--- blocks/acc-title/variants.js
+++ blocks/acc-title/variants.js
@@ -1,5 +1,31 @@
 /* eslint-disable */
 export default function getVariants() {
   return {
+      "heading_1": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "text-headline-2",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Heading 1",
+        "typography": "text-headline-1",
+        "text": "Heading 1",
+        "heading": "h1",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": ""
+      },
+      "heading_2": {
+        "jcr:primaryType": "nt:unstructured",
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "componentVariantTitle": "Heading 2",
+        "typography": "text-headline-2",
+        "text": "Heading 2",
+        "heading": "h2",
+        "jcr:lastModified": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "_charset_": "UTF-8",
+        "typographyMobile": ""
+      }
   };
 }
