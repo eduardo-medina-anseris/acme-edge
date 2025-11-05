804778c33c304b9ef730b75db0153f4f8d15c815
eduardo.medina <eduardo.medina@net-effect.com>
2025-11-05 14:01:06 -0600

    Separator support for UE

--- blocks/acc-separator/acc-separator.js
+++ blocks/acc-separator/acc-separator.js
@@ -4,26 +4,45 @@ import {
 
 export default function decorate(block) {
   const blockSettings = {};
+  const expectedKeys = ['separation', 'variant']; // orden esperado si no hay keys
 
   const pairContainers = block.querySelectorAll(':scope > div');
 
+  let hasKeys = false;
+
   pairContainers.forEach((container) => {
     const paragraphs = container.querySelectorAll('p');
+    if (paragraphs.length >= 2) hasKeys = true;
+  });
 
-    if (paragraphs.length >= 2) {
+  pairContainers.forEach((container, index) => {
+    const paragraphs = container.querySelectorAll('p');
+
+    if (hasKeys && paragraphs.length >= 2) {
       const key = paragraphs[0].textContent.trim();
       const value = paragraphs[1].textContent.trim();
-
       blockSettings[key] = value;
+    } else if (!hasKeys && paragraphs.length >= 1) {
+      const value = paragraphs[0].textContent.trim();
+      const key = expectedKeys[index] || `key${index}`;
+      blockSettings[key] = value;
+
+      if (key === 'separation') {
+        blockSettings[key] = `${value}px`;
+      } else {
+        blockSettings[key] = value;
+      }
     }
   });
+
   const hr = document.createElement('hr');
 
-  if (blockSettings.separation) {
-    hr.style.height = blockSettings.separation;
-  }
   const newBlock = document.createElement('div');
   applyVariantAttributes(newBlock, block);
+
+  if (blockSettings.separation) {
+    block.style.height = blockSettings.separation;
+  }
 
   const wrapperDiv = block.parentElement;
 
