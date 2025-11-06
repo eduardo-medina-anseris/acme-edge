d329b3556176760f3b56eb228fa73c16dc6e5793
eduardo.medina <eduardo.medina@net-effect.com>
2025-11-06 16:07:25 -0600

    Allow nested acc blocks

--- blocks/promotion/promotion.js
+++ blocks/promotion/promotion.js
@@ -1,24 +1,71 @@
 export default function decorate(block) {
-  // Create main hero wrapper
+  // Collect properties
+  const blockProperties = {};
+  const divContainers = block.querySelectorAll(':scope > div');
+
+  if (divContainers.length > 0) {
+    blockProperties.img = divContainers[0].querySelector('picture img');
+  }
+
+  if (divContainers.length > 1) {
+    blockProperties.title = divContainers[1].innerText?.trim();
+  }
+
+  if (divContainers.length > 2) {
+    blockProperties.text = divContainers[2].innerText?.trim();
+  }
+
+  blockProperties.buttons = [];
+  if (divContainers.length > 3) {
+    for (let i = 3; i < divContainers.length; i += 1) {
+      blockProperties.buttons.push(divContainers[i]);
+    }
+  }
+
+  // Build new markup
   const wrapper = document.createElement('div');
   wrapper.classList.add('hero-promotion-block');
 
-  // Content wrapper
-  const content = document.createElement('div');
-  content.classList.add('hero-promotion-content');
-
-  // BACKGROUND IMAGE: take the first <img> inside <picture> in first <p>
-  const img = block.querySelector(':scope > div:nth-child(1) p picture img');
-  if (img) {
+  if (blockProperties.img) {
     wrapper.style.backgroundImage = `
       linear-gradient(176.98deg, rgba(0, 0, 0, 0.6) 7.49%, rgba(0, 0, 0, 0) 95.15%),
-      url('${img.src}')
+      url('${blockProperties.img.src}')
     `;
     wrapper.style.backgroundSize = 'cover';
     wrapper.style.backgroundPosition = 'center';
   }
 
-  // Clear original block and insert new layout
-  // block.innerHTML = '';
-  // block.appendChild(wrapper);
+  const content = document.createElement('div');
+  content.classList.add('hero-promotion-content');
+  if (blockProperties.title || blockProperties.text) {
+    const promotionTexts = document.createElement('div');
+    promotionTexts.classList.add('hero-promotion-text');
+
+    const titleElem = document.createElement('h2');
+    titleElem.innerHTML = blockProperties.title;
+
+    const textElem = document.createElement('p');
+    textElem.innerHTML = blockProperties.text;
+
+    promotionTexts.appendChild(titleElem);
+    promotionTexts.appendChild(textElem);
+
+    content.appendChild(promotionTexts);
+  }
+
+  if (blockProperties.buttons.length > 0) {
+    const buttonsWrapper = document.createElement('div');
+    buttonsWrapper.classList.add('hero-promotion-buttons');
+
+    blockProperties.buttons.forEach((node) => {
+      buttonsWrapper.appendChild(node);
+    });
+
+    content.appendChild(buttonsWrapper);
+  }
+
+  wrapper.appendChild(content);
+
+  block.innerHTML = '';
+  block.appendChild(wrapper);
 }
