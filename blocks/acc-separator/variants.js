bbbddd8498cb47126ac8f67f49cd28819dae11b1
asruser <asruser@cm-p47002-e1212860-aem-author-6f65c894cd-6jsln>
2025-11-12 20:57:43 +0000

    Update theme styles - 2025-11-12 20:57:43

--- blocks/acc-separator/variants.js
+++ blocks/acc-separator/variants.js
@@ -2,48 +2,48 @@
 export default function getVariants() {
   return {
       "default": {
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "lineHeight": "",
         "separation": "40",
         "jcr:lastModified": "",
         "lineColor": "",
         "componentVariantTitle": "Default",
         "jcr:primaryType": "nt:unstructured",
         "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": ""
+        "jcr:lastModifiedBy": ""
       },
       "gray_line": {
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "lineHeight": "1",
         "separation": "20",
         "jcr:lastModified": "",
         "lineColor": "--grey-grey_400",
         "componentVariantTitle": "Gray Line",
         "jcr:primaryType": "nt:unstructured",
         "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": "1"
+        "jcr:lastModifiedBy": ""
       },
       "small": {
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "lineHeight": "",
         "separation": "20",
         "jcr:lastModified": "",
         "lineColor": "",
         "componentVariantTitle": "Small",
         "jcr:primaryType": "nt:unstructured",
         "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": ""
+        "jcr:lastModifiedBy": ""
       },
       "test": {
+        "sling:resourceType": "themes/components/elements/acc/separator",
+        "lineHeight": "1",
         "separation": "10",
         "jcr:lastModified": "",
         "lineColor": "--prm-primary_600",
         "componentVariantTitle": "Test",
         "jcr:primaryType": "nt:unstructured",
         "_charset_": "UTF-8",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/separator",
-        "lineHeight": "1"
+        "jcr:lastModifiedBy": ""
       }
   };
 }
--- blocks/acc-title/variants.js
+++ blocks/acc-title/variants.js
@@ -2,69 +2,69 @@
 export default function getVariants() {
   return {
       "heading_1": {
+        "typographyTablet": "text-headline-2",
+        "jcr:lastModifiedBy": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "jcr:primaryType": "nt:unstructured",
         "jcr:lastModified": "",
         "heading": "h1",
         "text": "Heading 1",
         "typography": "text-headline-1",
         "componentVariantTitle": "Heading 1",
         "typographyMobile": "text-headline-3",
-        "_charset_": "UTF-8",
-        "typographyTablet": "text-headline-2",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/title",
-        "jcr:primaryType": "nt:unstructured"
+        "_charset_": "UTF-8"
       },
       "heading_2": {
+        "typographyTablet": "",
+        "jcr:lastModifiedBy": "",
+        "sling:resourceType": "themes/components/elements/acc/title",
+        "jcr:primaryType": "nt:unstructured",
         "jcr:lastModified": "",
         "heading": "h2",
         "text": "Heading 2",
         "typography": "text-headline-2",
         "componentVariantTitle": "Heading 2",
         "typographyMobile": "",
-        "_charset_": "UTF-8",
+        "_charset_": "UTF-8"
+      },
+      "pretitle": {
         "typographyTablet": "",
         "jcr:lastModifiedBy": "",
         "sling:resourceType": "themes/components/elements/acc/title",
-        "jcr:primaryType": "nt:unstructured"
-      },
-      "pretitle": {
+        "jcr:primaryType": "nt:unstructured",
         "jcr:lastModified": "",
         "heading": "h4",
         "text": "Pretitle",
         "typography": "text-pretitle",
         "componentVariantTitle": "Pretitle",
         "typographyMobile": "",
-        "_charset_": "UTF-8",
+        "_charset_": "UTF-8"
+      },
+      "heading_3": {
         "typographyTablet": "",
         "jcr:lastModifiedBy": "",
         "sling:resourceType": "themes/components/elements/acc/title",
-        "jcr:primaryType": "nt:unstructured"
-      },
-      "heading_3": {
+        "jcr:primaryType": "nt:unstructured",
         "jcr:lastModified": "",
         "heading": "h3",
         "text": "Heading 3",
         "typography": "text-headline-3",
         "componentVariantTitle": "Heading 3",
         "typographyMobile": "",
-        "_charset_": "UTF-8",
+        "_charset_": "UTF-8"
+      },
+      "heading_4": {
         "typographyTablet": "",
         "jcr:lastModifiedBy": "",
         "sling:resourceType": "themes/components/elements/acc/title",
-        "jcr:primaryType": "nt:unstructured"
-      },
-      "heading_4": {
+        "jcr:primaryType": "nt:unstructured",
         "jcr:lastModified": "",
         "heading": "h4",
         "text": "Heading 4",
         "typography": "text-h4-large-text",
         "componentVariantTitle": "Heading 4",
         "typographyMobile": "",
-        "_charset_": "UTF-8",
-        "typographyTablet": "",
-        "jcr:lastModifiedBy": "",
-        "sling:resourceType": "themes/components/elements/acc/title",
-        "jcr:primaryType": "nt:unstructured"
+        "_charset_": "UTF-8"
       }
   };
 }
